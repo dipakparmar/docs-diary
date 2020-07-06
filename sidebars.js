21027a75a126c04173dd3640bec2754799853bd8
Dipak Parmar <hi@dipak.tech>
2020-07-06 14:55:05 -0700

    Added git-hooks to sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    Git: ['git-basic','git-gpg','git-reset'],
+    Git: ['git-basic','git-gpg','git-reset','git-hooks'],
     React: ['react-basic'],
     DNS: ['dns-dig']
   },
