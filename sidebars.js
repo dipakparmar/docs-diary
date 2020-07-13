db0ccbac6f056f13b51252c2e26922c09cb71052
Dipak Parmar <hi@dipak.tech>
2020-07-13 14:33:16 -0700

    Added emoji to sidebar 🤷🏻‍♂️

--- sidebars.js
+++ sidebars.js
@@ -1,13 +1,13 @@
 module.exports = {
   someSidebar: {
     Git: ["git/basic", "git/gpg", "git/reset", "git/hooks"],
-    Github: ["github/pages","github/actions"],
-    DNS: ["dns/dig"],
-    SSH: ["ssh/basic"],
-    Docker: ["docker/basics"],
-    Linux: ["linux/resync","linux/disk-management"],
-    Rancher: ["rancher/cli","rancher/host-installation", "rancher/single-node-installation"],
-    React: ["react/basic"],
+    "🐙 Github": ["github/pages","github/actions"],
+    "🔗 DNS": ["dns/dig"],
+    "🔐 SSH": ["ssh/basic"],
+    "🐳 Docker": ["docker/basics"],
+    "🐧 Linux": ["linux/resync","linux/disk-management"],
+    "🐮 Rancher": ["rancher/cli","rancher/host-installation", "rancher/single-node-installation"],
+    " ⚛️ React": ["react/basic"],
     "Awesome Lists": ["awesome/awesome-lists", "awesome/graphics-tools"]
   },
 };
