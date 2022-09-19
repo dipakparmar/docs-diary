366bb4dfdf82c2a0ccb04186310bcc23ede2ea2c
Dipak Parmar <hi@dipak.tech>
2022-09-18 19:44:27 -0700

    perf: add @type annotations and format it

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,138 +1,152 @@
 const isDeployPreview =
-  !!process.env.VERCEL_ENV === 'development' || !!process.env.VERCEL_ENV === 'preview';
+  !!process.env.VERCEL_ENV === "development" ||
+  !!process.env.VERCEL_ENV === "preview";
 
-module.exports = {
-  title: `Dipak's Docs Diary`,
-  tagline: '',
-  url: 'https://docs.dipak.tech',
-  baseUrl: '/',
-  trailingSlash: false,
-  favicon: 'img/favicon.ico',
-  organizationName: 'dipakparmar',
-  projectName: 'docs-diary',
-  scripts: [
-    {
-      src: 'https://cdn.splitbee.io/sb.js',
-      async: true,
-    },
-  ],
-  themeConfig: ({
-    image: 'https://opengraph.githubassets.com/5f64176affcb4de799b0eb3903e8e71e35d89e9022c4953e88f9b879302bead0/dipakparmar/docs-diary',
-    liveCodeBlock: {
-      playgroundPosition: 'bottom',
-    },
-    docs: {
-      sidebar: {
-        hideable: true,
-        autoCollapseCategories: true,
+// With JSDoc @type annotations, IDEs can provide config autocompletion
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+(
+  module.exports = {
+    title: `Dipak's Docs Diary`,
+    tagline: "",
+    url: "https://docs.dipak.tech",
+    baseUrl: "/",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    trailingSlash: false,
+    favicon: "img/favicon.ico",
+    organizationName: "dipakparmar",
+    projectName: "docs-diary",
+    scripts: [
+      {
+        src: "https://cdn.splitbee.io/sb.js",
+        async: true,
       },
-    },
-    colorMode: {
-      defaultMode: 'light',
-      disableSwitch: false,
-      respectPrefersColorScheme: true,
-    },
-    navbar: {
-      title: `Dipak's Docs Diary`,
-      hideOnScroll: true,
-      logo: {
-        alt: 'Docs Diary',
-        src: 'img/logo.svg',
-      },
-      items: [
-        // Left Navbar items
-        {
-          to: '/tags',
-          label: 'Tags',
-          position: 'left',
+    ],
+    themeConfig:
+      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+      ({
+        image:
+          "https://opengraph.githubassets.com/5f64176affcb4de799b0eb3903e8e71e35d89e9022c4953e88f9b879302bead0/dipakparmar/docs-diary",
+        liveCodeBlock: {
+          playgroundPosition: "bottom",
         },
-        // Right Navbar items
-        {
-          href: 'https://dipak.to/twitter?utm_source=docs.dipak.tech&utm_medium=nav_link&utm_campaign=dipaks_docs_diary',
-          position: 'right',
-          className: 'header-twitter-link',
-          'aria-label': 'Twitter',
+        docs: {
+          sidebar: {
+            hideable: true,
+            autoCollapseCategories: true,
+          },
         },
+        colorMode: {
+          defaultMode: "light",
+          disableSwitch: false,
+          respectPrefersColorScheme: true,
+        },
+        navbar: {
+          title: `Dipak's Docs Diary`,
+          hideOnScroll: true,
+          logo: {
+            alt: "Docs Diary",
+            src: "img/logo.svg",
+          },
+          items: [
+            // Left Navbar items
+            {
+              to: "/tags",
+              label: "Tags",
+              position: "left",
+            },
+            // Right Navbar items
+            {
+              href: "https://dipak.to/twitter?utm_source=docs.dipak.tech&utm_medium=nav_link&utm_campaign=dipaks_docs_diary",
+              position: "right",
+              className: "header-twitter-link",
+              "aria-label": "Twitter",
+            },
+            {
+              href: "https://github.com/dipakparmar/docs-diary?utm_source=docs.dipak.tech&utm_medium=nav_link",
+              position: "right",
+              className: "header-github-link",
+              "aria-label": "GitHub repository",
+            },
+          ],
+        },
+        footer: {
+          style: "dark",
+          copyright: `Copyright © ${new Date().getFullYear()} Dipak's Docs Diary. Built with Docusaurus.`,
+        },
+        algolia: {
+          appId: "NOKG2EH40V",
+          apiKey: "6711f63fe1e318da1f4bb8b811d59df3",
+          indexName: "dipak",
+          contextualSearch: false,
+        },
+      }),
+    themes: [],
+    plugins: [
+      [
+        require.resolve("@docusaurus/plugin-client-redirects"),
         {
-          href: 'https://github.com/dipakparmar/docs-diary?utm_source=docs.dipak.tech&utm_medium=nav_link',
-          position: 'right',
-          className: 'header-github-link',
-          'aria-label': 'GitHub repository',
+          createRedirects(existingPath) {
+            if (existingPath.includes("/")) {
+              // Redirect from /docs/X to /X
+              return [existingPath.replace("/", "/docs/")];
+            }
+            return undefined; // Return a falsy value: no redirect created
+          },
         },
       ],
-    },
-    footer: {
-      style: 'dark',
-      copyright: `Copyright © ${new Date().getFullYear()} Dipak's Docs Diary. Built with Docusaurus.`,
-    },
-    algolia: {
-      appId: 'NOKG2EH40V',
-      apiKey: '6711f63fe1e318da1f4bb8b811d59df3',
-      indexName: 'dipak',
-      contextualSearch: false,
-    },
-  }),
-  themes: [],
-  plugins: [[require.resolve("@docusaurus/plugin-client-redirects"), {
-    createRedirects(existingPath) {
-      if (existingPath.includes('/')) {
-        // Redirect from /docs/X to /X 
-        return [
-          existingPath.replace('/', '/docs/'),
-        ];
-      }
-      return undefined; // Return a falsy value: no redirect created
-    },
-  }], [require.resolve("@dipakparmar/docusaurus-plugin-umami"),
-  /** @type {import('@dipakparmar/docusaurus-plugin-umami').Options} */
-  (
-    {
-      websiteID: "c401b94a-f278-46e3-bc17-72e9494f1375",
-      analyticsDomain: "analytics.dipak.io",
-      scriptName: "ua.js",
-      dataDoNotTrack: true,
-      dataDomains: "docs.dipak.tech",
-    }),],
-  async function tailwindcss(context, options) {
-    return {
-      name: 'docusaurus-tailwindcss',
-      configurePostCss(postCssOptions) {
-        // Appends TailwindsCSS and AutoPrefixer
-        postCssOptions.plugins.push(require("tailwindcss"));
-        postCssOptions.plugins.push(require("autoprefixer"));
-        return postCssOptions;
-      }
-    }
-  }],
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          routeBasePath: '/',
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/dipakparmar/docs-diary/edit/main/',
-          showLastUpdateAuthor: true,
-          showLastUpdateTime: true,
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-        gtag: !isDeployPreview
-          ? {
-            trackingID: 'G-EZ7C3BQQ3J',
-            anonymizeIP: false,
-          }
-          : undefined,
-        sitemap: {
-          changefreq: 'daily',
-          priority: 0.5,
-          ignorePatterns: ['/tags/**'],
-        },
+      [
+        require.resolve("@dipakparmar/docusaurus-plugin-umami"),
+        /** @type {import('@dipakparmar/docusaurus-plugin-umami').Options} */
+        ({
+          websiteID: "c401b94a-f278-46e3-bc17-72e9494f1375",
+          analyticsDomain: "analytics.dipak.io",
+          scriptName: "ua.js",
+          dataDoNotTrack: true,
+          dataDomains: "docs.dipak.tech",
+        }),
+      ],
+      async function tailwindcss(context, options) {
+        return {
+          name: "docusaurus-tailwindcss",
+          configurePostCss(postCssOptions) {
+            // Appends TailwindsCSS and AutoPrefixer
+            postCssOptions.plugins.push(require("tailwindcss"));
+            postCssOptions.plugins.push(require("autoprefixer"));
+            return postCssOptions;
+          },
+        };
       },
-
     ],
-  ],
-};
+    presets: [
+      [
+        "@docusaurus/preset-classic",
+        /** @type {import('@docusaurus/preset-classic').Options} */
+        ({
+          docs: {
+            routeBasePath: "/",
+            sidebarPath: require.resolve("./sidebars.js"),
+            // Please change this to your repo.
+            editUrl: "https://github.com/dipakparmar/docs-diary/edit/main/",
+            showLastUpdateAuthor: true,
+            showLastUpdateTime: true,
+          },
+          theme: {
+            customCss: require.resolve("./src/css/custom.css"),
+          },
+          gtag: !isDeployPreview
+            ? {
+                trackingID: "G-EZ7C3BQQ3J",
+                anonymizeIP: false,
+              }
+            : undefined,
+          sitemap: {
+            changefreq: "daily",
+            priority: 0.5,
+            ignorePatterns: ["/tags/**"],
+          },
+        }),
+      ],
+    ],
+  }
+);
